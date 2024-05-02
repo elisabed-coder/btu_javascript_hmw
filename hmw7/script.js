abb3e7a4a14f4ecdfbc7287d3e9a9ddb2ddb05ff
eli <ebezhiasvhili@gmail.com>
2024-05-02 11:30:32 -0700

    hmw7

---
+++ hmw7/script.js
@@ -0,0 +1,38 @@
+async function fetchData() {
+  try {
+    const serverResponse = await fetch(
+      "https://handlers.education.launchcode.org/static/planets.json"
+    );
+
+    const planets = await serverResponse.json();
+
+    const destination = document.querySelector(".destination");
+    const userContainer = document.querySelector(".user");
+
+    let currentIndex = 0;
+
+    function showPlanet(index) {
+      if (index < 0 || index >= planets.length) {
+        return;
+      }
+      const planet = planets[index];
+      destination.innerHTML = `
+          <h1>${planet.name}</h1>
+          <img src="${planet.image}">
+        `;
+      currentIndex = index;
+    }
+
+    showPlanet(currentIndex);
+
+    // Event listener for clickable next button
+    destination.addEventListener("click", () => {
+      currentIndex = (currentIndex + 1) % planets.length;
+      showPlanet(currentIndex);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+fetchData();
